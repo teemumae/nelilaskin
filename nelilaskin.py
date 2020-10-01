77a6a8199596fb0193101e3a11dc1bc71d1ca0af
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-01 20:30:22 +0300

    Valmis?

--- nelilaskin.py
+++ nelilaskin.py
@@ -1,1 +1,23 @@
 
+operaatio = input("Valitse operaatio (+, -, *, /):")
+if operaatio != "+" and operaatio != "-" and operaatio != "*" and operaatio != "/":
+    print("Operaatiota ei ole olemassa")
+else:
+    try:
+        luku_1 = float(input("Anna luku 1:"))
+        luku_2 = float(input("Anna luku 2:"))
+    except ValueError:
+        print("Ei tämä ole mikään luku")
+    else:
+        if operaatio == "+":
+            summa = luku_1 + luku_2
+            print(summa)
+        elif operaatio=="-":
+            summa = luku_1 - luku_2
+            print(summa)
+        elif operaatio == "*":
+            summa = luku_1 * luku_2
+            print(summa)
+        elif operaatio == "/":
+            summa = luku_1 / luku_2
+            print(summa)
